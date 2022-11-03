24f63958e38723cc100678748470209fc2bad17f
lenoah-chacko <lenoahchacko@gmail.com>
2022-11-04 03:03:02 +0530

    integrated graph api in dashboard

--- src/components/admin/dashboard/graph/graph.js
+++ src/components/admin/dashboard/graph/graph.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Chart as ChartJS,
@@ -10,43 +10,54 @@ import {
   Tooltip,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
 
 ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip);
 
-export const options = {
-  responsive: true,
-  plugins: {
-      legend: {
-        display: false
+
+export function Graph() {
+  const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+          display: false
+        },
+        title: {
+        display: true,
+        text: 'Recipes added in the last 6 months',
+        font: {
+          size: 30
+        }
       },
-      title: {
-      display: true,
-      text: 'Recipes added in the last 6 months',
-      font: {
-        size: 30
-      }
     },
-  },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
+  };
+  
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', "October", "November", "December"];
+  
+  const [data,setData]= useState()
+  const [labels,setLabels]= useState()
+  
+  async function getData(){
+    console.log("getting data")
+      const response = await fetch("http://localhost:4000/api/admin/get-recent-recipes")
+      await response.json().then((data)=>{
+        setData(data.map((data)=>{return data.numberOfRecipes}))
+        setLabels(data.map((data)=>{return months[data.MonthNumber-1]}))
+      console.log("got",data)
+    })
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+  useEffect(()=>{
+    console.log("data",data)
+  },[data])
+  return <Line options={options} data={{"labels":labels,datasets: [
     {
       label: 'Submissions',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 5 })),
+      data: data,
       borderColor: 'rgb(0, 42, 50)',  // line graph's line colour
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     }
   ],
-};
-
-export function Graph() {
-  useEffect(()=>{
-    console.log(data.datasets[0].data)
-  })
-  return <Line options={options} data={data} />;
+}} />;
 }
